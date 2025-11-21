8dab0b1639c6ac53cdb441e2a58394212a71c3ae
ogmayne <augusterwema@outlook.com>
2025-11-21 13:02:11 +0200

    Add age your calculator project

---
+++ age_calculator.py
@@ -0,0 +1,6 @@
+# Age calculator program
+
+name = input("what is your name? ")
+birth_year = int(input("what year were you born? "))
+age = 2025 - birth_year
+print(f"Hello {name}, you are about {age} years old.")
